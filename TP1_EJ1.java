49e767506cede30eaed03a454691da4b14951b2f
ignaciobeling <ignacio_23b@hotmail.com>
2017-05-03 18:00:42 -0300

    Add files via upload

---
+++ TP1_EJ1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_ej1;
+
+/**
+ *
+ * @author IGNACIO
+ */
+public class TP1_EJ1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double area;
+        double radio;
+        radio = 25;
+        area= radio * radio * 3.14159;
+        System.out.println("el area de dicho circulo con radio "+ radio + " es "+ area);
+    }
+}
